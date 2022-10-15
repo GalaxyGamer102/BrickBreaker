3d4cdd271b387641c8edda3e513c568b27acd92a
Anthony Okoye <aokoye@lr1038.local>
2022-10-15 11:42:48 -0400

    Connected GamePlay to Main

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main{String[] args}(
         
+        JFrame frame = New JFrame();
+        GamePlay gamePlay = New GamePlay();
+
+        frame.setBounds(500,100,700,600);
+        frame.setTitle("brick :)");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gamePlay);
+        frame.setVisible(true);
     )
 }
---
+++ MapGenerator.java
@@ -0,0 +1,5 @@
+public class MapGenerator(
+    public MapGenerator(){
+        
+    }
+)
